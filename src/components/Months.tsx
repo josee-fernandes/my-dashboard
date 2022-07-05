2cf55a74d48061e913fd97811372b4291c825740
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2022-07-05 20:38:27 -0300

    Add month button created, not functional

--- src/components/Months.tsx
+++ src/components/Months.tsx
@@ -9,9 +9,14 @@ export const Months: React.FC = () => {
 
   return (
     <div className="col-span-3 row-span-2 shadow-2xl rounded-md p-10">
-      <h3 className="mb-5">
-        Meses
-      </h3>
+      <div className="flex justify-between mb-5">
+        <h3>
+          Meses
+        </h3>
+        <button className="bg-violet-500 hover:bg-violet-700 transition-all text-zinc-100 font-bold px-4 rounded-md">
+          Adicionar mês +
+        </button>
+      </div>
       <div className="flex flex-col">
         {months?.map((month) => (
           <Month
